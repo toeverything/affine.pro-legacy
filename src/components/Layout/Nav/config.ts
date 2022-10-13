2376d2efc74bc05b346707faa6fb9a4675edf862
JimmFly <yangjinfei001@gmail.com>
2022-10-13 13:33:28 +0800

    optimize code

--- src/components/Layout/Nav/config.ts
+++ src/components/Layout/Nav/config.ts
@@ -1,29 +1,10 @@
 import { useTranslation } from "react-i18next";
 
 export const useNavLink = () => {
-  const { t } = useTranslation();
-  return [
-    {
-      href: "/aboutUs",
-      title: t("AboutUs"),
-    },
-    {
-      href: "/blog",
-      title: t("Blog"),
-    },
-    {
-      href: "https://docs.affine.pro/",
-      title: t("Docs"),
-    },
-    {
-      href: "https://feedback.affine.pro/",
-      title: t("Feedback"),
-    },
-    {
-      href: "https://livedemo.affine.pro/",
-      title: t("Try it Online"),
-    },
-  ];
+  const leftNavLink = useLeftNavLink();
+  const rightNavLink = useRightNavLink();
+  const allNavLink = leftNavLink.concat(rightNavLink);
+  return allNavLink;
 };
 export const useLeftNavLink = () => {
   const { t } = useTranslation();
