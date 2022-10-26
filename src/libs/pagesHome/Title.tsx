2f58060be44b648e7e5b389dba04a67b069920a3
QiShaoXuan <qishaoxuan777@gmail.com>
2022-10-26 12:25:54 +0800

    feat: add pathfinder link

--- src/libs/pagesHome/Title.tsx
+++ src/libs/pagesHome/Title.tsx
@@ -1,11 +1,22 @@
+import type { MouseEvent } from "react";
+import { useState, useRef } from "react";
 import styled from "@emotion/styled";
 import { useTranslation } from "react-i18next";
 import Alternatives from "./Alternatives";
 import GithubSvg from "./GithubIcon";
 import Logo from "./Logo";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+
 const Title = () => {
   const { t } = useTranslation();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const anchorElRef = useRef<HTMLButtonElement>(null);
+  const open = Boolean(anchorEl);
 
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
       <StyledMain>
@@ -27,22 +38,69 @@ const Title = () => {
             &nbsp;{t("Check GitHub")}
           </a>
         </StyledGithub>
-        <StyledLogo>
-          <a
+        <StyledLogo
+          ref={anchorElRef}
+          id="try-button"
+          onMouseEnter={(event: MouseEvent<HTMLButtonElement>) => {
+            setAnchorEl(anchorElRef.current);
+          }}
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+        >
+          <div>
+            <Logo fill="#fff" stroke="#fff" width="20" height="20" />
+            &nbsp;{t("Try it Online")}
+          </div>
+        </StyledLogo>
+      </StyledButton>
+
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        MenuListProps={{
+          "aria-labelledby": "try-button",
+          onMouseLeave: handleClose,
+        }}
+        autoFocus={false}
+      >
+        <MenuItem onClick={handleClose}>
+          <StyledLink
             href="https://livedemo.affine.pro/"
             target="_blank"
             rel="noreferrer"
           >
-            <Logo fill="#fff" stroke="#fff" width="20" height="20" />
-            &nbsp;{t("Try it Online")}
-          </a>
-        </StyledLogo>
-      </StyledButton>
+            AFFINE Pre-Alpha
+          </StyledLink>
+        </MenuItem>
+        <MenuItem onClick={handleClose} sx={{ cursor: "auto" }}>
+          <div>
+            <StyledLink
+              href="https://pathfinder.affine.pro/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              AFFINE Alpha
+              <StyledBadge>New</StyledBadge>
+            </StyledLink>
+
+            <StyledSubLink
+              href="https://affine.pro/content/blog/affine-alpha-is-coming/index"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Why a new Version?
+            </StyledSubLink>
+          </div>
+        </MenuItem>
+      </Menu>
     </>
   );
 };
 
 export default Title;
+
 const StyledMain = styled.div({
   display: "flex",
   flexDirection: "column",
@@ -133,4 +191,26 @@ const StyledLogo = styled.button({
   border: "none",
   margin: "auto 24px",
   padding: "16px 28px",
+});
+const StyledBadge = styled.div({
+  backgroundColor: "#ff1744",
+  color: "#fff",
+  transform: "translate(0,-50%)",
+  fontSize: "10px",
+  padding: "0 4px",
+  height: "16px",
+  display: "inline-flex",
+  justifyContent: "center",
+  alignItems: "center",
+  borderRadius: "4px 4px 4px 0",
+});
+const StyledLink = styled.a({
+  color: "#000",
+  fontSize: "18px",
+  fontWeight: "bold",
+});
+const StyledSubLink = styled.a({
+  color: "#096bde",
+  fontSize: "14px",
+  marginLeft: "5px",
 });
