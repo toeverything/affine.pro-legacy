7415b7a635a4c8301db3f474b02501d4abfce35a
JimmFly <yangjinfei001@gmail.com>
2022-11-02 13:24:13 +0800

    add in navbar

--- src/components/Layout/Nav/NormalHeader.tsx
+++ src/components/Layout/Nav/NormalHeader.tsx
@@ -1,4 +1,6 @@
+import Popper from "@/libs/common/Popper";
 import styled from "@emotion/styled";
+import { useRef, useState } from "react";
 import { LinkText } from "../LinkText";
 import {
   useFeedbackNavLink,
@@ -20,21 +22,51 @@ export const NormalLeftHeader = () => {
   );
 };
 export const NormalRightHeader = () => {
-  const feedBackLink = useFeedbackNavLink();
+  const feedbackLink = useFeedbackNavLink();
   const liveDemoLink = useLiveDemoNavLink();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const anchorElRef = useRef<HTMLDivElement>(null);
+  const open = Boolean(anchorEl);
+  const isHover = useRef(false);
+  const handleClose = () => {
+    isHover.current = false;
+    setTimeout(() => {
+      if (!isHover.current) {
+        setAnchorEl(null);
+      }
+    }, 50);
+  };
+  const handleOpen = () => {
+    isHover.current = true;
+    setAnchorEl(anchorElRef.current);
+  };
+
   return (
     <>
       <StyledContainer>
-        {feedBackLink.map((nav) => {
+        {feedbackLink.map((nav) => {
           return <LinkText key={nav.title} href={nav.href} title={nav.title} />;
         })}
-        <div>
+        <div
+          ref={anchorElRef}
+          onMouseOver={handleOpen}
+          onMouseLeave={handleClose}
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+        >
           {liveDemoLink.map((nav) => {
             return (
               <LinkText key={nav.title} href={nav.href} title={nav.title} />
             );
           })}
         </div>
+        <Popper
+          anchorEl={anchorEl}
+          open={open}
+          handleClose={handleClose}
+          handleOpen={handleOpen}
+        />
         <LanguageMenu />
       </StyledContainer>
     </>
